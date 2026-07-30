0c83851da81fa4579683892488232ca0ddd08e20
Hennadii Panasiuk <gennapanasuk@gmail.com>
2026-07-30 15:16:29 +0200

    added validation for the creation product form

--- app/Http/Requests/StoreProductRequest.php
+++ app/Http/Requests/StoreProductRequest.php
@@ -12,7 +12,7 @@ class StoreProductRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -23,7 +23,19 @@ class StoreProductRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'name' => ['required', 'string', 'min:10'],
+            'brand' => ['required', 'string', 'min:5'],
+            'category_id' => ['required', 'exists:categories,id'],
+            'price' => ['required', 'numeric'],
+            'weight' => ['required', 'numeric'],
+            'description' => ['required', 'string']
+        ];
+    }
+
+    public function attributes(): array
+    {
+        return [
+            'category_id' => 'Category'
         ];
     }
 }
